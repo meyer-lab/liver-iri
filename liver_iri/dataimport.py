d80a03053cbf76eb63bc10274d6dfd7677925d30
Cyrillus <cyztan@gmail.com>
2022-07-22 16:57:11 -0700

    Import cytokine data as a tensor; misc other setups

---
+++ liver_iri/dataimport.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import xarray as xr
+import numpy as np
+
+def cytokine_data():
+    df = pd.read_csv("./liver_iri/data/cytokine_20201120.csv")
+    data = xr.DataArray(coords={
+        "Patient": pd.unique(df["PID"]),
+        "Visit Type": pd.unique(df["Visit Type"]),
+        "Cytokine": df.columns[6:],
+        },
+        dims=["Patient", "Visit Type", "Cytokine"]
+    )
+
+    # log standardize data
+    df.iloc[:, 6:] = np.log(df.iloc[:, 6:]).replace(-np.inf, 0)
+    df.iloc[:, 6:] -= np.mean(df.iloc[:, 6:], axis=0)
+    df.iloc[:, 6:] /= np.std(df.iloc[:, 6:], axis=0)
+
+    for rrow in df.iterrows():
+        data.loc[rrow[1]["PID"], rrow[1]["Visit Type"], :] = rrow[1][6:]
+    return data
