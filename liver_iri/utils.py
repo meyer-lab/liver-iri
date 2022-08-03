82646b27dd0ee06246c19393ae45ab298d9e62f9
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2022-08-03 16:55:54 -0700

    Added predict, moved reorder_table to utils  - Added regression, categorical predictions to predict.py  - Moved reorder_table to utils.py

---
+++ liver_iri/utils.py
@@ -0,0 +1,16 @@
+import scipy.cluster.hierarchy as sch
+
+
+def reorder_table(df):
+    """
+    Reorder a table's rows using hierarchical clustering.
+    Parameters:
+        df (pandas.DataFrame): data to be clustered; rows are treated as samples
+            to be clustered
+    Returns:
+        df (pandas.DataFrame): data with rows reordered via heirarchical
+            clustering
+    """
+    y = sch.linkage(df.to_numpy(), method='centroid')
+    index = sch.dendrogram(y, orientation='right', no_plot=True)['leaves']
+    return df.iloc[index, :]
