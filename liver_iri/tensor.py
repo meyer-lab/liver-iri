9a0d4fa52f46f58f18d147e7741d4bbacc099b24
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2022-08-04 11:03:04 -0700

    Added tensor.py for general tensor functions

---
+++ liver_iri/tensor.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+from tensorpack import perform_CP
+
+from .dataimport import cytokine_data
+
+
+def get_factors(rank=9):
+    data = cytokine_data()
+    cp = perform_CP(data.values, rank)
+    factors = pd.DataFrame(
+        cp.factors[0],
+        index=data.Patient.values,
+        columns=np.arange(1, rank + 1)
+    )
+    return factors
