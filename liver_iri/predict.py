3a40d3ea88b828b8dfa36207eeae2ad57bac4a5c
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2023-01-10 16:28:13 -0800

    Adds RF, SVM classification models and hyperparamter-optimization  - Adds RF and SVM classification models to predict  - Adds basic grid-search hyperparameter optimization

--- liver_iri/predict.py
+++ liver_iri/predict.py
@@ -1,12 +1,16 @@
 import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import ElasticNet, ElasticNetCV, LogisticRegression, \
     LogisticRegressionCV
+from sklearn.metrics import balanced_accuracy_score
 from sklearn.model_selection import cross_val_predict, LeaveOneOut, \
-    StratifiedKFold
+    RepeatedStratifiedKFold
+from sklearn.svm import SVC
 
-skf = StratifiedKFold(
-    n_splits=10
+skf = RepeatedStratifiedKFold(
+    n_splits=5,
+    n_repeats=5
 )
 
 
@@ -116,3 +120,168 @@ def predict_categorical(data, labels, return_coef=False):
         return np.max(scores), model, coef
     else:
         return np.max(scores), model
+
+
+def optimize_parameters(classifier, data, labels, params):
+    """
+    Optimizes parameters for provided classifier.
+
+    Parameters:
+        classifier (sklearn.estimator): sklearn classifier
+        data (pandas.DataFrame): Data to predict
+        labels (pandas.Series): Labels for provided data
+        params (dict[str:Iterable]): Maps parameter name to values to test
+
+    Returns:
+        params (dict[str:Iterable]): Parameters that lead to highest accuracy
+            via cross-validation
+    """
+    key = list(params.keys())[0]
+    grid = np.zeros(
+        shape=[len(params[key])]
+    )
+    for index, param in enumerate(params[key]):
+        model = classifier(**{key: param})
+        scores = []
+        for train_index, test_index in skf.split(data, labels):
+            model.fit(
+                data.iloc[train_index, :],
+                labels.iloc[train_index]
+            )
+            predicted = model.predict(data.iloc[test_index, :])
+            scores.append(
+                balanced_accuracy_score(
+                    labels.iloc[test_index],
+                    predicted
+                )
+            )
+        grid[index] = np.mean(scores)
+
+    return {
+        key: params[key][int(np.argmax(grid))],
+    }
+
+
+def predict_categorical_rf(data, labels, return_coef=False):
+    """
+    Fits Random Forest classifier model and hyperparameters to provided data.
+
+    Parameters:
+        data (pandas.DataFrame): Data to predict
+        labels (pandas.Series): Labels for provided data
+        return_coef (bool, default: False): Return model coefficients
+
+    Returns:
+        score (float): Accuracy for best-performing model
+        model (sklearn.LogisticRegression): Fitted model with hyperparameters
+            optimized to predict provided data and labels
+        return_coef (numpy.array): LR coefficients for each feature
+    """
+    if isinstance(labels, pd.Series):
+        labels = labels.reset_index(drop=True)
+    else:
+        labels = pd.Series(labels)
+
+    labels = labels[labels != 'Unknown']
+
+    if isinstance(data, pd.Series):
+        data = data.iloc[labels.index]
+        data = data.values.reshape(-1, 1)
+    elif isinstance(data, pd.DataFrame):
+        data = data.iloc[labels.index, :]
+    else:
+        data = data[labels.index, :]
+
+    best_params = optimize_parameters(
+        RandomForestClassifier,
+        data,
+        labels,
+        {'n_estimators': np.arange(50, 160, 10)}
+    )
+    model = RandomForestClassifier(**best_params)
+    scores = np.zeros(shape=(skf.cvargs['n_splits'] * skf.n_repeats))
+    coefs = np.zeros(
+        shape=(
+            skf.cvargs['n_splits'] * skf.n_repeats,
+            data.shape[1]
+        )
+    )
+    i = 0
+    for train_index, test_index in skf.split(data, labels):
+        model.fit(
+            data.iloc[train_index, :],
+            labels.iloc[train_index]
+        )
+        predicted = model.predict(data.iloc[test_index, :])
+        scores[i] = \
+            balanced_accuracy_score(
+                labels.iloc[test_index],
+                predicted
+            )
+
+        coefs[i, :] = model.feature_importances_
+        i += 1
+
+    score = np.mean(scores)
+    coefs = np.mean(coefs, axis=0)
+
+    if return_coef:
+        return score, model, coefs
+    else:
+        return score, model
+
+
+def predict_categorical_svc(data, labels):
+    """
+    Fits SVC model and hyperparameters to provided data.
+
+    Parameters:
+        data (pandas.DataFrame): Data to predict
+        labels (pandas.Series): Labels for provided data
+
+    Returns:
+        score (float): Accuracy for best-performing model
+        model (sklearn.LogisticRegression): Fitted model with hyperparameters
+            optimized to predict provided data and labels
+        return_coef (numpy.array): LR coefficients for each feature
+    """
+    if isinstance(labels, pd.Series):
+        labels = labels.reset_index(drop=True)
+    else:
+        labels = pd.Series(labels)
+
+    labels = labels[labels != 'Unknown']
+
+    if isinstance(data, pd.Series):
+        data = data.iloc[labels.index]
+        data = data.values.reshape(-1, 1)
+    elif isinstance(data, pd.DataFrame):
+        data = data.iloc[labels.index, :]
+    else:
+        data = data[labels.index, :]
+
+    best_params = optimize_parameters(
+        SVC,
+        data,
+        labels,
+        {'C': np.logspace(-4, 4, 9)}
+    )
+    model = SVC(**best_params)
+    scores = np.zeros(shape=(skf.cvargs['n_splits'] * skf.n_repeats))
+    i = 0
+    for train_index, test_index in skf.split(data, labels):
+        model.fit(
+            data.iloc[train_index, :],
+            labels.iloc[train_index]
+        )
+        predicted = model.predict(data.iloc[test_index, :])
+        scores[i] = \
+            balanced_accuracy_score(
+                labels.iloc[test_index],
+                predicted
+            )
+
+        i += 1
+
+    score = np.mean(scores)
+    return score, model
