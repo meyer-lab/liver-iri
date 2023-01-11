43d4f8f9bbddf80b5673f6abfe85ee59c8224db4
Jackson Chin <74275962+JacksonLChin@users.noreply.github.com>
2023-01-10 17:47:36 -0800

    Renamed tensor functions, added basic CP  - Renamed tensor functions to better match project structure  - Added basic CP method to tensor

--- liver_iri/tensor.py
+++ liver_iri/tensor.py
@@ -1,12 +1,41 @@
 import numpy as np
+from tensorpack import perform_CP
 from tensorpack.coupled import CoupledTensor
+import xarray as xr
 
 from liver_iri.dataimport import build_coupled_tensor
 
 OPTIMAL_RANK = 6
 
 
-def get_factors(data=None, rank=OPTIMAL_RANK, nonneg=False):
+def run_cp(data, rank=OPTIMAL_RANK):
+    """
+    Runs CP on provided data.
+
+    Parameters:
+        data (xr.Dataset, default: None): coupled tensors to factorize; if
+            'None', builds default coupled tensor (see
+            dataimport.build_coupled_tensor)
+        rank (int, default: OPTIMAL_RANK): tensor factorization rank
+        nonneg (bool, default: False): runs non-negative factorization
+
+    Returns:
+        factors (pd.DataFrame): patient factors for provided data
+        decomposer (CoupledTensor): decomposition object
+    """
+    if isinstance(data, xr.Dataset):
+        data = data.to_array().squeeze()
+        data = data.to_numpy()
+    elif isinstance(data, xr.DataArray):
+        data = data.to_numpy()
+    elif not isinstance(data, np.ndarray):
+        raise TypeError('Unrecognized data format provided')
+
+    cp = perform_CP(data, rank)
+    return cp
+
+
+def run_coupled(data=None, rank=OPTIMAL_RANK, nonneg=False):
     """
     Runs coupled CP and returns factor matrices.
 
